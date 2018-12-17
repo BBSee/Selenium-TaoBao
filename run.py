8ce7817b8db1630b7124974ec86b1ae48aa0c285
hamdi <javs_shao@163.com>
2018-12-17 17:03:50 +0800

    程序入口文件

--- run.py
+++ run.py
@@ -1,29 +1,73 @@
 import pymongo
-
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from pyquery import PyQuery as pq
+from config import *
 from urllib.parse import quote
-from pyquery import PyQuery
 
 
-browser = webdriver.chrome()
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+browser = webdriver.Chrome(chrome_options=chrome_options)
+
 wait = WebDriverWait(browser, 10)
-KEYWORD = 'ipad'
-
-MONGO_URL = 'localhost'
-MONGO_DB = 'taobao'
-MONGO_COLLECTION = 'products'
 client = pymongo.MongoClient(MONGO_URL)
 db = client[MONGO_DB]
 
+
+def index_page(page):
+    """
+    抓取索引页
+    :param page: 页码
+    """
+    print('正在爬取第', page, '页')
+    try:
+        url = 'https://s.taobao.com/search?q=' + quote(KEYWORD)
+        browser.get(url)
+        if page > 1:
+            input = wait.until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager div.form > input')))
+            submit = wait.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, '#mainsrp-pager div.form > span.btn.J_Submit')))
+            input.clear()
+            input.send_keys(page)
+            submit.click()
+        wait.until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#mainsrp-pager li.item.active > span'), str(page)))
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.m-itemlist .items .item')))
+        get_products()
+    except TimeoutException:
+        index_page(page)
+
+
+def get_products():
+    """
+    提取商品数据
+    """
+    html = browser.page_source
+    doc = pq(html)
+    items = doc('#mainsrp-itemlist .items .item').items()
+    for item in items:
+        product = {
+            'image': item.find('.pic .img').attr('data-src'),
+            'price': item.find('.price').text(),
+            'deal': item.find('.deal-cnt').text(),
+            'title': item.find('.title').text(),
+            'shop': item.find('.shop').text(),
+            'location': item.find('.location').text()
+        }
+        print(product)
+        save_to_mongo(product)
+
+
 def save_to_mongo(result):
     """
-    保存结果到MongoDb数据库中
+    保存至MongoDB
     :param result: 结果
-    :return:
     """
     try:
         if db[MONGO_COLLECTION].insert(result):
@@ -32,72 +76,14 @@ def save_to_mongo(result):
         print('存储到MongoDB失败')
 
 
-def get_products():
-    """
-    提取商品数据
-    :return:
-    """
-    html = browser.page_source
-    doc = PyQuery(html)
-    items = doc('#mainsrp-itemlist .items .item').items()
-    for item in items:
-        prodcut = {
-            'image': item.find('.pic .img').attr('data-src'),
-            'price': item.find('.price').text(),
-            'deal': item.find('.deal-cnt').text(),
-            'title': item.find('.title').text(),
-            'shop': item.find('.shop').text(),
-            'location': item.find('.location').text()
-        }
-        print(prodcut)
-        save_to_mongo(prodcut)
-
-
-def index_page(page):
-    """
-    抓取索引页
-    :param page:
-    :return:
-    """
-    print('正在爬取第:', page, "页")
-    try:
-        url = 'https://s.taobao.com/search?q=' + quote(KEYWORD)
-        browser.get(url)
-        if page > 1:
-            input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager div.form > input')))
-            submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#mainsrp-pager div.form > span.btn.J_Submit')))
-            input.clear()
-            input.send_keys(page)
-            submit.click()
-        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#mainsrp-pager li.item.active > span'), str(page)))
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.m-itemlist .items .item')))
-        get_products()
-    except TimeoutException:
-        index_page(page)
-
-MAX_PAGE = 100
 def main():
     """
     遍历每一页
-    :return:
     """
     for i in range(1, MAX_PAGE + 1):
         index_page(i)
     browser.close()
 
+
 if __name__ == '__main__':
     main()
-
-
-
-# from selenium.webdriver import ActionChains
-#
-# action = ActionChains(driver)
-#
-# source = driver.find_element_by_xpath("//*[@id='nc_1_n1t']/span")  # 需要滑动的元素
-# action.click_and_hold(source).perform()   # 鼠标左键按下不放
-# action.move_by_offset(298, 0)  # 需要滑动的坐标
-# action.release().perform()  # 释放鼠标
-#
-# time.sleep(1)
-
